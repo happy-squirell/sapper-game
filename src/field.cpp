3b290167d4890e93ca12d71bed7b4fee2b1cc51e
frost <amirhansdikov3751@gmail.com>
2026-05-16 11:57:30 +0300

    функция open_box выдавала segmentation fault. необходимо переделать

--- src/field.cpp
+++ src/field.cpp
@@ -7,18 +7,6 @@
 
 void open_box(int x, int y){
 
-    if(mines_pos[x][y] == 0) user_visible[x][y] = '0';
-    else user_visible[x][y] = '*';
-
-    if(mines_pos[x][y] == 0){
-        for(int ax = -1; ax <= 1; ax++){
-            for(int ay = -1; ay <= 1; ay++){
-                if(ax == 0 && ay == 0) continue;
-
-                open_box(x + ax, y + ay);
-            }
-        }
-    }
 }
 
 void print_field(){
